7989f908a7aa964ad808adf9c685762d43b25a7e
yi.shi <kukudeshiyi@yeah.net>
2022-05-08 17:08:26 +0800

    feat: update

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,17 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import styles from "./styles.module.css";
-import talks from "./homePageTalk";
+
+const talks = [
+  {
+    title: "今天把博客改了改，也算能放出来见人了",
+    author: "ShiYi",
+    time: "2022-05-08",
+    content:
+      "想着也是该搭个博客了，写点东西，不管是工作还是生活，总要记录下嘛！所以博客也分了生活以及技术两块区域，希望可以坚持下去，也算是生活的痕迹。",
+    image: "homeTalkImg/homePageImage.jpeg",
+  },
+].reverse();
 
 function HomeContent() {
   return (
